a6c9934632bd45d8675328806d353d4c5062c46f
francislu2021 <85589987+francislu2021@users.noreply.github.com>
2021-11-23 19:43:12 -0500

    delete json stringfy for testing reason

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -28,12 +28,6 @@ const Register = () => {
 
 </div>
 </div>
-
-<div className="row">
-    <pre>
-        {JSON.stringify({name, email, password}, null, 4)}
-    </pre>
-</div>
  </div>
  </div>
     );

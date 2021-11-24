99cd290f07cb6f15a8a0e2dddd3335902d3a4591
francislu2021 <85589987+francislu2021@users.noreply.github.com>
2021-11-23 21:45:25 -0500

    c12-axios and toast notifications setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -9,6 +10,8 @@ function App() {
   return (
     <Router> 
       <Nav />
+      <Toaster position="bottom-center" toastOptions={{
+        duration:2000}}/>
     <Routes>
 <Route exact path="/" element={<Home />}></Route>
 <Route exact path="/register" element={<Register />}></Route>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,29 @@
 import React, {useState} from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import axios from "axios";
+import {toast} from "react-hot-toast";
 
 const Register = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleClick = async () => {
-        console.log(name, email, password);
+    const handleClick = async (e) => {
+        //console.log(name, email, password);
+        try {
+            e.preventDefault();
+            const {data} = await axios.post("/register", {
+                name,
+                email,
+                password,
+            });
+            console.log(data)
+            toast.success("Registration successful. Please log in!");
+        } catch (err) {
+            console.log(err)
+            toast.error("Something wrong. Try again!");
+        }
     };
 
     return (
